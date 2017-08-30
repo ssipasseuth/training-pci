ba7c3b41cc98dc4fc7337d79a72bf7af5f41ca34
Sam Sipasseuth <sam@taotesting.com>
2017-08-30 10:26:48 +0200

    step 3 adds styling and media assets

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -44,5 +44,10 @@ class Updater extends \common_ext_ExtensionUpdater
             call_user_func(new RegisterPciSampleA(), ['0.2.0']);
             $this->setVersion('0.3.0');
         }
+
+        if ($this->isVersion('0.3.0')) {
+            call_user_func(new RegisterPciSampleA(), ['0.3.0']);
+            $this->setVersion('0.4.0');
+        }
     }
 }
